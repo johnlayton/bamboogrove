37a76dfc3f601103b16876a9cc7352ea37b4efd9
john layton <johnstewartlayton@gmail.com>
2019-10-21 22:44:21 +1100

    add install script

---
+++ bin/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+#
+# Kube checks
+#
+
+# kube machine
+# kube create adrastea
+
+#
+# Minikube checks
+#
+
+# minikube status
+# minikube dashboard
+# minikube addons enable ingress
+# kubectl get pods -n kube-system
+
+#
+# kubectl checks
+#
+# kubectl cluster-info
+
+#mkdir data
+
+#minikube mount $(pwd)/data:/
+
+kube create
